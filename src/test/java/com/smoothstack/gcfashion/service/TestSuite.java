497415f3d876f2a0925925ebeecf1472ce51dfbe
jalvesteffer <jason.alvesteffer@smoothstack.com>
2020-09-22 01:44:21 -0400

    Added swagger and test suite class

---
+++ src/test/java/com/smoothstack/gcfashion/service/TestSuite.java
@@ -0,0 +1,11 @@
+package com.smoothstack.gcfashion.service;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ FindTest.class, GetTest.class, PaymentTest.class, SaveTransactionTest.class, StoreServiceTest.class, UpdateTransactionTest.class })
+public class TestSuite {
+
+}
